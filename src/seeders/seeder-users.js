1c96bb54123575c8b390f72f930a7321d92dda10
NVQuay <tsebyuaq20111998@gmail.com>
2023-07-10 15:51:06 +0700

    create model, micration

--- src/seeders/seeder-users.js
+++ src/seeders/seeder-users.js
@@ -5,9 +5,14 @@ module.exports = {
   async up(queryInterface, Sequelize) {
     return queryInterface.bulkInsert("Users", [
       {
-        firstName: "John",
-        lastName: "Doe",
-        email: "example@example.com",
+        email: "admin@email.com",
+        password: "123123",
+        firstName: "quaynguyen",
+        lastName: "dev",
+        address: "hcm",
+        gender: 1,
+        typeRole: "ROLE",
+        keyRole: "R1",
         createdAt: new Date(),
         updatedAt: new Date(),
       },
